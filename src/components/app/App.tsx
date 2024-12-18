aaea400c6e636d54c5be39368926ed5d798b414c
Un5had0wed <atulganesh2002@gmail.com>
2024-12-19 00:33:46 +0530

    defined the layout of the game

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,15 +1,22 @@
 import type { FC } from "preact/compat";
+import { Chat } from "../chat/Chat";
 import { GameRoot } from "../gameRoot/GameRoot";
 import { Header } from "../header/Header";
 import { Layout } from "../layout/Layout";
 import { Main } from "../main/Main";
+import { PlayingArea } from "../playinArea/PlayingArea";
+import { ScoreBoard } from "../scoreBoard/ScoreBoard";
 
 export const App: FC = () => {
   return (
     <GameRoot>
       <Layout>
-        <Header></Header>
-        <Main></Main>
+        <Header>Doodle Duel</Header>
+        <Main>
+          <ScoreBoard />
+          <PlayingArea />
+          <Chat />
+        </Main>
       </Layout>
     </GameRoot>
   );
---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,6 @@
+import type { FC } from "preact/compat";
+import styles from './Chat.module.css';
+
+export const Chat: FC = () => {
+    return <div className={styles.chat}></div>
+}
